a8c353830bcdc48d94429cd823f19b716547be6a
Sudip Adhikari <sudeep_sudeep07@hotmail.com>
2019-05-07 09:03:17 -0700

    simple counter complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import React from 'react';
 
 
+const Header = () =>{
+  return(
+    <header>
+      <h1>Simple counter</h1>
+    </header>
+  );
+};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class Counter extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+handleCountAdd = e =>{
+  e.preventDefault();
+  let count = this.state.count
+  count++;
+  this.setState({count});
+};
+
+handleCountSubtract = e =>{
+  e.preventDefault();
+  let count = this.state.count
+  count--;
+  this.setState({count});
+};
+
+render(){
+  return(
+    <div>
+      <p>The current count is {this.state.count}</p>
+      <button onClick = {this.handleCountAdd}>Click to add to the counter</button>
+      <button onClick = {this.handleCountSubtract}>Click to subtract from the counter</button>
     </div>
   );
+}
+}
+
+class App extends React.Component{
+  render(){
+    return(
+    <React.Fragment>
+      <Header/>
+      <Counter/>
+    </React.Fragment>
+    );
+  }
 }
 
 export default App;
